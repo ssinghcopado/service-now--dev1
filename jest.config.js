8e688f35996499058e6612e979566cf4c488ab30
Subhabrata Pal <64831415+Subha-git-2020@users.noreply.github.com>
2023-01-19 11:59:13 +0530

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    moduleFileExtensions: ['js', 'ts'],
+    testEnvironment: 'node',
+    testMatch: ['**/*.test.ts', '**/*.test.js'],
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+    transformIgnorePatterns: ['^.+\\.js$'],
+    verbose: true,
+    roots: ["<rootDir>/src/", "<rootDir>/records/", "<rootDir>/scripts/"]
+}
