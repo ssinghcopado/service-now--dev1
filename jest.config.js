8db217b1d916279cdb0e8b7c7cf84f6702f5e99b
Subhabrata Pal <64831415+Subha-git-2020@users.noreply.github.com>
2023-08-01 12:21:23 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,7 @@
 module.exports = {
     moduleFileExtensions: ['js', 'ts'],
     testEnvironment: 'jsdom',
-    testEnvironmentOptions: {
-    // Add any necessary options for jsdom here
-    // Example options:
-    resources: "usable", // Enable loading external resources (e.g., images) in jsdom
-    runScripts: "dangerously", // Allow scripts to run in jsdom
-    pretendToBeVisual: true, // Pretend to be a visual browser, enables CSS layout
-    virtualConsole: true, // Enable virtual console (console.log, console.error) in jsdom
-    userAgent: "Custom User-Agent String", // Set a custom User-Agent string for jsdom
-    // ... any other jsdom options you may need
-  },
+    testEnvironmentOptions: ['',''],
     testMatch: ['**/*.test.ts', '**/*.test.js'],
     transform: {
         '^.+\\.ts$': 'ts-jest',
