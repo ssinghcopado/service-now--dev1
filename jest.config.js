0557634bdaa50555d6b8a299919c8a9219a745d3
Subhabrata Pal <64831415+Subha-git-2020@users.noreply.github.com>
2023-08-03 16:36:38 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     moduleFileExtensions: ['js', 'ts'],
-    testEnvironment: 'node',
+    testEnvironment: 'jsdom',
     testMatch: ['**/*.test.ts', '**/*.test.js'],
     transform: {
         '^.+\\.ts$': 'ts-jest',
