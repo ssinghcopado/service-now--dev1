d48e67e960c54f4c981cc454d2aa2eaf740d51da
Subhabrata Pal <64831415+Subha-git-2020@users.noreply.github.com>
2023-08-03 10:54:20 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,6 @@
 module.exports = {
     moduleFileExtensions: ['js', 'ts'],
     testEnvironment: 'jsdom',
-    testEnvironmentOptions: {
-    // Add any custom options you want to pass to the test environment
-    customExportConditions: ['jsdom', 'jsdom-addons'],
-    // Add other test environment options here if needed
-  },
     testMatch: ['**/*.test.ts', '**/*.test.js'],
     transform: {
         '^.+\\.ts$': 'ts-jest',
